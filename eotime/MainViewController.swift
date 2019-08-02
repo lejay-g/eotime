329c81d984067ebbdfafd1c02f4baf7eb7f1a83e
catherine <chocolat.lejay@gmail.com>
2019-08-02 13:12:45 +0900

    時刻が更新されるようにした

--- eotime/MainViewController.swift
+++ eotime/MainViewController.swift
@@ -19,15 +19,56 @@ class MainViewController: UIViewController {
     @IBOutlet weak var hour: UILabel!
     @IBOutlet weak var minute: UILabel!
     
+    //時計
+    private var timer = Timer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //現在のunix時刻を取得
-        let timeInterval = NSDate().timeIntervalSince1970
+        self.disptimer()
+       
+    }
+
+    //時刻を進める
+    func disptimer() {
+        if !timer.isValid {
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.updateTimer), userInfo: nil, repeats: true)
+        }
         
+    }
+    
+    @objc private func updateTimer() {
+        //現在のunix時刻を取得
+        let unixtime = NSDate().timeIntervalSince1970
+        
+        get_hour(utime: unixtime)
+    }
+
+    func get_hour(utime: TimeInterval){
         //エオルゼア時間60分=リアル時間175秒
-        let et_seconds = timeInterval * 60.0 / 175.0
-        let et_hours = timeInterval / 175.0
+        let et_seconds = utime * 60.0 / 175.0
+        let et_hours = utime / 175.0
+        //時
+        let disp_et_hour = Int(et_hours.truncatingRemainder(dividingBy: 24.0))
+        //分
+        let disp_et_minute = Int(et_seconds.truncatingRemainder(dividingBy: 60.0))
+        
+//        //debug
+//        year.text = String(disp_et_year)
+//        month.text = String(disp_et_month)
+//        day.text = String(disp_et_day)
+//        hour.text = String(disp_et_hour)
+//        minute.text = String(disp_et_minute)
+        
+        //disp
+        time_hour.text = String(format: "%02d", disp_et_hour)
+        time_minute.text = String(format: "%02d", disp_et_minute)
+        
+    }
+    
+    func get_date(utime: TimeInterval) -> String {
+        //エオルゼア時間60分=リアル時間175秒
+        let et_hours = utime / 175.0
         //エオルゼア1日24時間
         let et_days = et_hours / 24.0
         //エオルゼア1ヶ月32日
@@ -39,24 +80,12 @@ class MainViewController: UIViewController {
         let disp_et_month = Int(et_months.truncatingRemainder(dividingBy: 12.0) + 1.0)
         //日
         let disp_et_day = Int(et_days.truncatingRemainder(dividingBy: 32.0) + 1.0)
-        //時
-        let disp_et_hour = Int(et_hours.truncatingRemainder(dividingBy: 24.0))
-        //分
-        let disp_et_minute = Int(et_seconds.truncatingRemainder(dividingBy: 60.0))
         
-        //debug
-        year.text = String(disp_et_year)
-        month.text = String(disp_et_month)
-        day.text = String(disp_et_day)
-        hour.text = String(disp_et_hour)
-        minute.text = String(disp_et_minute)
-        
-        //disp
-        time_hour.text = String(disp_et_hour)
-        time_minute.text = String(disp_et_minute)
-        
-    }
+        let ret_disp = String(disp_et_year) + "-" + String(disp_et_month) + "-" + String(disp_et_day)
 
+        return ret_disp
+
+    }
     /*
     // MARK: - Navigation
 
